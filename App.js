2b8e80b2abd86f29016f8ba3299041a00175c88d
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-02-21 12:23:46 -0300

    commitdata

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
         <Text style={[styles.numero, {color:'black'}]}>21</Text>
       <Text style={[styles.sala, {backgroundColor:'pink'}]}>I2HN</Text>
       <Text style={[styles.texto, {color:'blue'}]}>João Pedro Araújo Teixeira</Text>
+      <Text style={[styles.texto2, {color:'red', backgroundColor:'blue'}]}>21/02/2024</Text>
     </View>
   );
 }
@@ -25,6 +26,16 @@ const styles = StyleSheet.create({
    justifyContent: 'center',
    left: '30%',
   },
+  texto2: {
+    fontSize: 20, 
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: '30%',
+    borderBottomColor: 'red',
+    borderBottomWidth: 2,
+    marginBottom: 30,
+   },
   numero: {
     fontSize: 25, 
     fontStyle: 'italic',
