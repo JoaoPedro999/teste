fdfb8133fcd0b4930080b6fa5c27d204342477f2
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-02-21 09:13:22 -0300

    commit01

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color:'red'}}>Open up App.js to start working on your app!</Text>
+      <Text style={[styles.texto, {color:'blue'}]}>Estilização de linha por grupo ( StyleSheet)</Text>
     </View>
   );
 }
@@ -13,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgray',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  texto: {
+   fontSize: 32, 
   },
 });
